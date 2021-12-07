d762b6353d80f5cffc743511259b6da9ba8f29cd
ovookt <ovo_230@mail.ru>
2021-12-07 17:43:31 +0500

    first commit

---
+++ app/src/main/java/com/company/exercise4/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.company.exercise4
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavigationMenu: BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        bottomNavigationMenu = findViewById(R.id.bottom_navigation_menu)
+
+        bottomNavigationMenu.setOnItemSelectedListener { item ->
+            var fragment: Fragment? = null
+            when (item.itemId) {
+                R.id.page1 -> {
+                    fragment = Fragment1()
+
+                }
+                R.id.page2 -> {
+                    fragment = Fragment2()
+                }
+            }
+            replaceFragment(fragment!!)
+            true
+        }
+
+        bottomNavigationMenu.selectedItemId = R.id.page1
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .addToBackStack(fragment.tag)
+            .commit()
+    }
+
+}
+
